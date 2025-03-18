9ec1f42aec5860c9a8003bf9827aa2a821aa8d08
vikram <vikramkunwar1404@gmail.com>
2025-03-18 19:28:09 +0530

    Local Notifcation with Badge

--- LocalPushNotification/ViewController.swift
+++ LocalPushNotification/ViewController.swift
@@ -8,12 +8,77 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+           super.viewDidLoad()
+           requestNotificationPermission()
+           
+          
+           NotificationCenter.default.addObserver(self,
+                                                 selector: #selector(appBecameActive),
+                                                 name: UIApplication.didBecomeActiveNotification,
+                                                 object: nil)
+           resetBadgeCount()
+       }
+       
+       @objc func appBecameActive() {
+           resetBadgeCount()
+       }
+    
+    // Request permission for notifications
+    func requestNotificationPermission() {
+            let center = UNUserNotificationCenter.current()
+            center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+                if granted {
+                    print("Notification permission granted")
+                } else {
+                    print("Notification permission denied")
+                }
+            }
+        }
+    
+    
+    @IBAction func pressNotificatioBtn(_ sender: UIButton){
+        
+        scheduleNotification()
     }
+    
+    func scheduleNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Hello!"
+        content.body = "This is your notification after 3 seconds."
+        content.sound = UNNotificationSound.default
 
+        
+        UNUserNotificationCenter.current().getPendingNotificationRequests { requests in
+            let pendingCount = requests.count + 1
+            content.badge = NSNumber(value: pendingCount)
+            
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
 
+            UNUserNotificationCenter.current().add(request) { error in
+                if let error = error {
+                    print("Error adding notification: \(error.localizedDescription)")
+                } else {
+                    print("Notification scheduled with badge count: \(pendingCount)")
+                }
+            }
+        }
+    }
+        
+    func resetBadgeCount() {
+        UNUserNotificationCenter.current().setBadgeCount(0) { error in
+            if let error = error {
+                print("Error resetting badge count: \(error.localizedDescription)")
+            } else {
+                print("Badge count reset to 0")
+                
+            }
+        }
+    }
 }
+
+
+
 
